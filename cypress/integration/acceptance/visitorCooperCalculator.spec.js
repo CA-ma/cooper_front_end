ee7279489bf24e8002082689adba89ae2e50ad79
Max Aubain <max.aubain@gmail.com>
2019-08-07 21:11:25 +0200

    Adds age and distance inputs and gender select to App component, corrects typo in 01 feature test

--- cypress/integration/acceptance/visitorCooperCalculator.spec.js
+++ cypress/integration/acceptance/visitorCooperCalculator.spec.js
@@ -2,7 +2,7 @@ describe('Visitor can input metrics and get Cooper Calculator result', () => {
   before(function() {
     cy.visit('http://localhost:3001');
     cy.get('input[id="distance"]').type('1000');
-    cy.get('selector[id="gender"]').select('female');
+    cy.get('select[id="gender"]').select('female');
     cy.get('input[id="age"]').type('23');
   });
 
