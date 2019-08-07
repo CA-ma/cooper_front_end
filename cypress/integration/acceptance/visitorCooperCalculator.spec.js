21663f79e6ffa15e85bb7dcf7f5a142dffd4f56b
Max Aubain <max.aubain@gmail.com>
2019-08-07 16:43:59 +0200

    Adds tests to 01 feature test to see results and metrics

--- cypress/integration/acceptance/visitorCooperCalculator.spec.js
+++ cypress/integration/acceptance/visitorCooperCalculator.spec.js
@@ -5,4 +5,20 @@ describe('Visitor can input metrics and get Cooper Calculator result', () => {
     cy.get('selector[id="gender"]').select('female');
     cy.get('input[id="age"]').type('23');
   })
+
+  it('should see distance', () => {
+    cy.contains('23 y/o');
+  })
+
+  it('should see gender', () => {
+    cy.contains('female');
+  })
+
+  it('should see age', () => {
+    cy.contains('running 1000 meters');
+  })
+
+  it('should see result', () => {
+    cy.contains('Result: Poor');
+  })
 })
