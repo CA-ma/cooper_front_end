fc191acb24ba7077b86eedf1c7da22168cbb9171
Max Aubain <max.aubain@gmail.com>
2019-08-08 20:28:31 +0200

    Adds tests for CooperCalculator, 1 - test CC output as a function, 2 - test CC output as a component, 3 - test CC props as a component

--- src/__tests__/cooperCalculator.test.js
+++ src/__tests__/cooperCalculator.test.js
@@ -4,9 +4,20 @@ import { shallow } from 'enzyme';
 import CooperCalculator from '../modules/CooperCalculator'
 
 describe('CooperCalculator returns result', () => {
-  it('return poor', () => {
-    const describedModule = shallow(<CooperCalculator age = '23' gender = 'female' distance = '1000' />)
-    const moduleReturn = 'poor'
-    expect(describedModule.text()).toEqual(moduleReturn)
+  it('return poor with function', () => {
+    const describedModule = CooperCalculator("23", "female", "1000");
+    const moduleReturn = 'poor';
+    expect(describedModule).toEqual(moduleReturn);
+  })
+
+  it('return poor with module', () => {
+    const describedModule = shallow(<CooperCalculator age="23" gender="female" distance="1000"/>);
+    const moduleReturn = 'poor';
+    expect(describedModule.text()).toEqual(moduleReturn);
+  })
+
+  it('return poor with module', () => {
+    const describedModule = shallow(<CooperCalculator age="23" gender="female" distance="1000"/>);
+    expect(describedModule.props()).toEqual(true);
   })
 })
