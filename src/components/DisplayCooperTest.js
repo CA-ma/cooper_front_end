5ae2208077cff45847877f81c9b76b77e053571d
Max Aubain <max.aubain@gmail.com>
2019-08-08 11:10:05 +0200

    Adds import of CooperCalculator and populates calculate method with CooperCalculator reference

--- src/components/DisplayCooperTest.js
+++ src/components/DisplayCooperTest.js
@@ -1,34 +1,31 @@
-import React, { Component} from 'react';
+import React, { Component } from "react";
+import CooperCalculator from "../modules/CooperCalculator";
 
 class DisplayCooperResult extends Component {
-  
   calculate() {
-
+    return CooperCalculator(
+      this.props.age,
+      this.props.gender,
+      this.props.distance
+    );
   }
 
   render() {
+    let result;
 
-    let result
-
-    if (this.props.age !== '' && this.props.distance !== '') {
-      result = 
+    if (this.props.age !== "" && this.props.distance !== "") {
+      result = (
         <div>
           <p>
-            For a { this.props.age } year-old { this.props.gender } running a distance of { this.props.distance },
+            For a {this.props.age} year-old {this.props.gender} running a
+            distance of {this.props.distance},
           </p>
-          <p>
-            the Cooper Test result is: { this.calculate }.
-          </p>
-        </div>;
+          <p>the Cooper Test result is: {this.calculate}.</p>
+        </div>
+      );
     }
-
-
-    return (
-      <div>
-        { result }
-      </div>
-    )
+    return <div>{result}</div>;
   }
 }
 
-export default DisplayCooperResult
+export default DisplayCooperResult;
