158e44a366fef5be4db5b8548df0298035dec71a
Max Aubain <max.aubain@gmail.com>
2019-08-08 13:15:40 +0200

    Corrects argument paratheses in displayCooperResult.test.js

--- src/__tests__/displayCooperResult.test.js
+++ src/__tests__/displayCooperResult.test.js
@@ -4,17 +4,17 @@ import { shallow } from 'enzyme';
 import DisplayCooperResult from '../components/DisplayCooperResult';
 
 describe('DisplayCooperResult component', () => {
-  it('evaluates display female, poor result'), () => {
+  it('evaluates display female, poor result', () => {
     const describedComponent = shallow(<DisplayCooperResult age = '23' gender = 'female' distance = '1000' />)
     const componentReturn = <p>result is: poor</p>
     expect(describedComponent.contains(componentReturn)).toEqual(true)
-  }
+  })
 
-  it('evaluates display female, average result'), () => {
+  it('evaluates display female, average result', () => {
     const describedComponent = shallow(<DisplayCooperResult age = '23' gender = 'female' distance = '2000' />)
     const componentReturn = <p>result is: average</p>
     expect(describedComponent.contains(componentReturn)).toEqual(true)
-  }
+  })
 })
 
 
