428c59130de36cab39ff20a246ba679aedf49ea0
Max Aubain <max.aubain@gmail.com>
2019-08-08 09:59:12 +0200

    Adds shell unit test file displayCooperResult.test.js

---
+++ src/__tests__/displayCooperResult.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import DisplayCooperResult from '../components/DisplayCooperResult';
+
+describe('DisplayCooperResult component', () => {
+  it('evaluates to display female, poor result'), () => {
+
+  }
+
+  it('evaluates to display female, average result'), () => {
+    
+  }
+})
+
+
