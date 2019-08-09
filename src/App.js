d33eefc5aa3f2d2b96dd9735de07756485ac8911
Max Aubain <max.aubain@gmail.com>
2019-08-09 14:10:47 +0200

    To App component, adds method to show login button and login form, adds comments for each section of functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-{/* Import libraries */}
+// Import libraries
 import React, { Component } from "react";
 
-{/* Import components and modules */}
+// Import components and modules
 import DisplayCooperResult from "./components/DisplayCooperResult";
 import InputFields from "./components/InputFields";
 import LoginForm from "./components/LoginForm";
@@ -10,7 +10,7 @@ class App extends Component {
   constructor() {
     super();
 
-    {/* App state parameters */}
+    {/* App state parameter asignments */}
     {/* DisplayCooperResult, InputFields: age, gender, distance */}
     {/* LoginForm: renderLoginForm */}
     this.state = { 
@@ -25,20 +25,31 @@ class App extends Component {
   }
 
   render() {
+    {/* Method to render Login button, and LoginForm upon Login button click */}
+    let renderLogin
+    if (this.state.renderLoginForm === true) {
+      renderLogin = (<LoginForm />)
+    } else {
+      renderLogin = (<button id="login" onClick = {() => {this.setState({renderLoginForm: true})}}>Login</button>)
+    }
+
     return (
       <div>
+        {/* Render Cooper Test and input fields explanation */}
         <p>Please enter your biometrics and run distance to see the corresponding <a href="https://en.wikipedia.org/wiki/Cooper_test" >Cooper Test</a> result.</p>
+        <p>The "distance" field should receive the approximate distance you can run in 12 minutes.</p>
         <InputFields inputChangeHandler={this.onChange.bind(this)} />
 
+        {/* Render Cooper Test result calculated from above inputs*/}
         <DisplayCooperResult
           age={this.state.age}
           gender={this.state.gender}
           distance={this.state.distance}
         />
 
+        {/* render user login info here */}
         <hr></hr>
-        <button id="login">Login</button>
-        <LoginForm />
+        {renderLogin}
       </div>
     )
   }
