0489e0a08b9920de1bc4433b4376830d7d2d3bd7
Max Aubain <max.aubain@gmail.com>
2019-08-09 12:30:14 +0200

    Adds acceptance test for user login using a button and form

--- cypress/integration/acceptance/userLoginAuthentication.spec.js
+++ cypress/integration/acceptance/userLoginAuthentication.spec.js
@@ -1,9 +1,23 @@
 describe ('User can login', () => {
   it('successfully', () => {
-
+    cy.visit('http://localhost:3001')
+    cy.get('#login').click()
+    cy.get('#login-form').within(() => {
+      cy.get('#email').type('user@email.com')
+      cy.get('#password').type('password')
+      cy.get('button').click()
+    })
+    cy.contains('Hi user@email.com.')
   })
 
   it('with invalid credentials', () => {
-
+    cy.visit('http://localhost:3001')
+    cy.get('#login').click()
+    cy.get('#login-form').within(() => {
+      cy.get('#email').type('user@email.com')
+      cy.get('#password').type('wrongpassword')
+      cy.get('button').click()
+    })
+    cy.contains('Invalid username or password.')
   })
 })
